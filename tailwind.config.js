9f50e6ae228af8421faac3ae5899096f7b5648cd
sowhat13 <pahat98@gmail.com>
2020-09-30 02:18:28 +0300

    debug

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.html'],
-  },
+  purge: [],
   theme: {
     extend: {},
   },
